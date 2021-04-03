6dd388ce1918b541c1f057eeb537c837de856b45
Amartya Aishwarya <amartya970914@gmail.com>
2021-04-03 21:52:31 +0530

    add new task-wip

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { List } from 'src/app/models/list.model';
+import { Task } from 'src/app/models/task.model';
 import { TaskService } from 'src/app/task.service';
 
 @Component({
@@ -8,23 +10,30 @@ import { TaskService } from 'src/app/task.service';
   styleUrls: ['./task-view.component.scss'],
 })
 export class TaskViewComponent implements OnInit {
-  lists: any[];
-  tasks: any[];
+  lists: List[];
+  tasks: Task[];
   constructor(
     private taskService: TaskService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.taskService.getLists().subscribe((lists: any[]) => {
+    this.taskService.getLists().subscribe((lists: List[]) => {
       this.lists = lists;
     });
 
     this.route.params.subscribe((params: Params) => {
       // console.log(params);
-      this.taskService.getTasks(params.listId).subscribe((tasks: any[]) => {
+      this.taskService.getTasks(params.listId).subscribe((tasks: Task[]) => {
         this.tasks = tasks;
       });
+    });
+  }
+
+  onTaskClick(task: Task) {
+    //we want to set the task to complete
+    this.taskService.complete(task).subscribe(() => {
+      console.log('completed success');
     });
   }
 }
--- frontend/src/app/web-request.service.ts
+++ frontend/src/app/web-request.service.ts
@@ -19,7 +19,9 @@ export class WebRequestService {
   }
 
   patch(uri: string, payload: Object) {
-    return this.http.patch(`${this.ROOT_URL}/${uri}`, payload);
+    return this.http.patch(`${this.ROOT_URL}/${uri}`, payload, {
+      responseType: 'text',
+    });
   }
 
   delete(uri: string) {
