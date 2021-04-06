2c71a33d0f12b233b298724326d80bb7c938cf04
Amartya Aishwarya <amartya970914@gmail.com>
2021-04-07 00:05:22 +0530

    user auth functions

--- api/db/models/user.model.js
+++ api/db/models/user.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
 
 //JWT secret
 const jwtSecret = "wdfinifviwfb242345320r892fbsdkfvbwr23r";
@@ -105,12 +106,50 @@ UserSchema.statics.findByIdAndToken = function (_id, token) {
 
 UserSchema.statics.findByCredentials = function (email, password) {
   let User = this;
+  return User.findOne({ email }).then((user) => {
+    if (!user) return Promise.reject();
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) resolve(user);
+        else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+UserSchema.statics.hasRefreshTokenExpired = (expiresAt) => {
+  let secondsSinceEpoch = Date.now() / 1000;
+  if (expiresAt > secondsSinceEpoch) {
+    // hasnt expired
+    return false;
+  } else {
+    // has expired
+    return true;
+  }
 };
 
 // MIDDLEWARE
 // Before a user document is saved, this code runs
 UserSchema.pre("save", function (next) {
   let user = this;
+  let costFactor = 10;
+
+  if (user.isModified("password")) {
+    // if the password field has been edited/changed then run this code
+
+    // generate salt and hash password
+    bcrypt.genSalt(costFactor, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 // HELPER METHODS
@@ -138,3 +177,7 @@ let generateRefreshTokenExpiryTime = () => {
   let secondsUntilExpire = daysUntilExpire * 24 * 60 * 60;
   return Date.now() / 1000 + secondsUntilExpire;
 };
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
