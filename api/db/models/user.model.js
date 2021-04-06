90507f1d7b838506dcec4673428e25767fd8e580
Amartya Aishwarya <amartya970914@gmail.com>
2021-04-06 21:31:10 +0530

    npm install bcryptjs --save

--- api/db/models/user.model.js
+++ api/db/models/user.model.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+//JWT secret
+const jwtSecret = "wdfinifviwfb242345320r892fbsdkfvbwr23r";
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -34,4 +39,102 @@ UserSchema.methods.toJSON = function () {
 
   // return the document except the password and sessions (these shouldn't be made available)
   return _.omit(userObject, ["password", "sessions"]);
+};
+
+UserSchema.methods.generateAccessAuthToken = function () {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    //Create the JSON Web token and return that
+    jwt.sign(
+      { _id: user._id.toHexString() },
+      jwtSecret,
+      { expiresIn: "15m" },
+      (err, token) => {
+        if (!err) {
+          resolve(token);
+        } else {
+          // there is an error
+          reject();
+        }
+      }
+    );
+  });
+};
+
+UserSchema.methods.generateRefreshAuthToken = function () {
+  // This method simply generates a 64byte hex string - it doesn't save it to the database. saveSessionToDatabase() does that
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(64, (err, buf) => {
+      if (!err) {
+        // no error
+        let token = buf.toString("hex");
+
+        return resolve(token);
+      }
+    });
+  });
+};
+
+UserSchema.methods.createSession = function () {
+  let user = this;
+
+  return user
+    .generateRefreshAuthToken()
+    .then((refreshToken) => {
+      return saveSessionToDatabase(user, refreshToken);
+    })
+    .then((refreshToken) => {
+      // saved to database successfully
+      // now return the refresh token
+      return refreshToken;
+    })
+    .catch((err) => {
+      return Promise.reject("Failed to save session to database. \n" + err);
+    });
+};
+
+// MODEL METHODS (static methods)
+UserSchema.statics.findByIdAndToken = function (_id, token) {
+  // finds user by id and token
+  // used in auth middleware (verifysession)
+
+  const User = this;
+
+  return User.findOne({ _id, "sessions.token": token });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  let User = this;
+};
+
+// MIDDLEWARE
+// Before a user document is saved, this code runs
+UserSchema.pre("save", function (next) {
+  let user = this;
+});
+
+// HELPER METHODS
+let saveSessionToDatabase = (user, refreshToken) => {
+  // save session to database
+  return new Promise((resolve, reject) => {
+    let expiresAt = generateRefreshTokenExpiryTime();
+
+    user.sessions.push({ token: refreshToken, expiresAt: expiresAt });
+
+    user
+      .save()
+      .then(() => {
+        // saved session successfully
+        return resolve(refreshToken);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+let generateRefreshTokenExpiryTime = () => {
+  let daysUntilExpire = "10";
+  let secondsUntilExpire = daysUntilExpire * 24 * 60 * 60;
+  return Date.now() / 1000 + secondsUntilExpire;
 };
